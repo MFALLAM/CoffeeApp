eb9dc8dc565e59eaa2cf1ea5776328b8d9ad81c2
MFALLAM <mohammed.fathi.allam@gmail.com>
2022-10-15 20:13:19 +0300

    Added increment method & decrement method

--- app/src/main/java/com/example/coffee/MainActivity.java
+++ app/src/main/java/com/example/coffee/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.coffee;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     private int price = 5;
+    private int count = 0;
+
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,14 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView txtView = findViewById(R.id.text_total_qty);
         txtView.setText("" + number);
+    }
+
+    private void increment(View view, int count) {
+
+    }
+
+    private void decrement(int count) {
+
     }
 
     private void displayPrice(int number) {
